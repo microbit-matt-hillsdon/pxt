1bab9a2a18f504511c2423e5f2f7becb2f88c71b
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-08-08 14:52:37 +0100

    Controller mode that uses app cmds
    This allows an iframe embedder to take the same level of control as an
    app:
    
    - intercept downloads
    - intercept save
    - benefit from the "back" messages
    
    Also adds an iframe message to trigger compile.
    
    To avoid separately disabling WebUSB this mode also forces WebUSB off.
    If we didn't do this a queryVariant like "androidapp" would be needed.

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -5855,7 +5855,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     pxt.perf.measureEnd("setAppTarget");
 
     let theme = pxt.appTarget.appTheme;
-    const isControllerIFrame = (theme.allowParentController || pxt.shell.isControllerMode()) && pxt.BrowserUtils.isIFrame();
+    const isControllerIFrame = theme.allowParentController || pxt.shell.isControllerMode()
     // disable auth in iframe scenarios
     if (isControllerIFrame)
         pxt.auth.enableAuth(false);
