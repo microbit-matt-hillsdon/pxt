868fcf07b9fdbbd44180b1cbc0ed5fdd8cdae98f
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-02-26 15:35:21 +0000

    Temporarily remove flyout caching.
    This is really problematic for the keyboard nav plugin. The plugin assumes it
    can access the flyout's workspace at initialization time.
    
    There's a largely pointless delegation pattern here that we can remove if we
    keep this but likely we need to do something to address performance if it's
    still an issue. Hoping this is good enough for user testing at least.

--- pxtblocks/plugins/flyout/verticalFlyout.ts
+++ pxtblocks/plugins/flyout/verticalFlyout.ts
@@ -8,209 +8,96 @@ export class VerticalFlyout implements Blockly.IFlyout {
     autoClose = true;
     CORNER_RADIUS: number;
 
-    protected cached: CachedFlyout[] = [];
-    protected element: SVGElement;
-    protected activeFlyout: CachedFlyout;
-    protected visible: boolean;
-
-    protected dummyWorkspace: Blockly.WorkspaceSvg;
-    protected containerVisible: boolean;
+    protected activeFlyout: CachedFlyout
 
     constructor(protected options: Blockly.Options) {
-        this.dummyWorkspace = new Blockly.WorkspaceSvg(options);
-        this.dummyWorkspace.setVisible(false);
+        this.activeFlyout = new CachedFlyout(this.options)
+        this.activeFlyout.autoClose = this.autoClose;
     }
 
     createDom(tagName: string | Blockly.utils.Svg<SVGSVGElement> | Blockly.utils.Svg<SVGGElement>): SVGElement {
-        if (this.element) {
-            return this.element;
-        }
-
-        this.dummyWorkspace.createDom();
-
-        this.element = Blockly.utils.dom.createSvgElement(
-            tagName,
-            {
-                class: "blocklyFlyout"
-            }
-        );
-
-        this.element.id = "multiFlyout";
-        this.element.style.display = "none";
-
-        return this.element;
+        return this.activeFlyout.createDom(tagName);
     }
 
     init(targetWorkspace: Blockly.WorkspaceSvg): void {
         this.targetWorkspace = targetWorkspace;
+        this.activeFlyout.init(this.targetWorkspace);
     }
 
     dispose(): void {
-        for (const entry of this.cached) {
-            entry.dispose();
-        }
+        this.activeFlyout.dispose();
     }
 
     getWidth(): number {
-        if (this.activeFlyout) {
-            return this.activeFlyout.getWidth();
-        }
-
-        return 0;
+        return this.activeFlyout.getWidth();
     }
 
     getHeight(): number {
-        if (this.activeFlyout) {
-            return this.activeFlyout.getHeight();
-        }
-
-        return 0;
+        return this.activeFlyout.getHeight();
     }
 
     getWorkspace(): Blockly.WorkspaceSvg {
-        if (this.activeFlyout) {
-            return this.activeFlyout.getWorkspace();
-        }
-
-        return this.dummyWorkspace;
+        return this.activeFlyout.getWorkspace();
     }
 
     isVisible(): boolean {
-        return this.visible;
+        return this.activeFlyout.isVisible();
     }
 
     setVisible(visible: boolean): void {
-        if (visible === this.visible) return;
-
-        this.visible = visible;
-        this.element.style.display = visible ? "block"  : "none";
-
-        for (const flyout of this.cached) {
-            if (flyout !== this.activeFlyout && flyout.isVisible()) {
-                flyout.setVisible(false);
-            }
-        }
-
-        if (this.activeFlyout) {
-            this.activeFlyout.setVisible(visible);
-        }
+        this.activeFlyout.setVisible(visible);
     }
 
     setContainerVisible(visible: boolean): void {
-        if (this.containerVisible === visible) return;
-
-        this.containerVisible = visible;
-
-        for (const entry of this.cached) {
-            entry.setContainerVisible(visible);
-        }
+        this.activeFlyout.setContainerVisible(visible);
     }
 
     hide(): void {
-        if (this.activeFlyout) {
-            this.activeFlyout.hide();
-        }
-
-        this.setVisible(false);
+        this.activeFlyout.hide();
     }
 
     show(flyoutDef: string | Blockly.utils.toolbox.FlyoutDefinition, cacheKey?: string): void {
-        const hash = cacheKey || this.hashBlocks(flyoutDef as Element[]);
-
-        if (this.activeFlyout) {
-            this.activeFlyout.hide();
-        }
-
-        const existing = this.cached.find(e => e.key === hash);
-
-        this.element.style.display = "block";
-        this.visible = true;
-
-        if (existing) {
-            this.activeFlyout = existing;
-            this.activeFlyout.autoClose = this.autoClose;
-            this.activeFlyout.setContainerVisible(this.containerVisible);
-            this.activeFlyout.show(flyoutDef)
-            return;
-        }
-
-        this.activeFlyout = new CachedFlyout(this.options);
-        this.cached.push(this.activeFlyout);
-        this.element.appendChild(this.activeFlyout.createDom("g"));
-        this.activeFlyout.init(this.targetWorkspace);
-
-        this.activeFlyout.key = hash;
-        this.activeFlyout.autoClose = this.autoClose;
-        this.activeFlyout.setContainerVisible(this.containerVisible);
         this.activeFlyout.show(flyoutDef);
     }
 
     createBlock(originalBlock: Blockly.BlockSvg): Blockly.BlockSvg {
-        if (this.activeFlyout) {
-            return this.activeFlyout.createBlock(originalBlock);
-        }
-
-        return undefined;
+        return this.activeFlyout.createBlock(originalBlock);
     }
 
     reflow(): void {
-        if (this.activeFlyout) {
-            this.activeFlyout.reflow();
-        }
+        this.activeFlyout.reflow();
     }
 
     isScrollable(): boolean {
-        if (this.activeFlyout) {
-            return this.activeFlyout.isScrollable();
-        }
-
-        return true;
+        return this.activeFlyout.isScrollable();
     }
 
     getX(): number {
-        if (this.activeFlyout) {
-            return this.activeFlyout.getX();
-        }
-        return 0;
+        return this.activeFlyout.getX();
     }
 
     getY(): number {
-        if (this.activeFlyout) {
-            return this.activeFlyout.getY();
-        }
-        return 0;
+        return this.activeFlyout.getY();
     }
 
     position(): void {
-        if (this.activeFlyout) {
-            this.activeFlyout.position();
-        }
+        this.activeFlyout.position();
     }
 
     isDragTowardWorkspace(currentDragDeltaXY: Blockly.utils.Coordinate): boolean {
-        if (this.activeFlyout) {
-            return this.activeFlyout.isDragTowardWorkspace(currentDragDeltaXY);
-        }
-
-        return false;
+        return this.activeFlyout.isDragTowardWorkspace(currentDragDeltaXY);
     }
 
     isBlockCreatable(block: Blockly.BlockSvg): boolean {
-        if (this.activeFlyout) {
-            return this.activeFlyout.isBlockCreatable(block);
-        }
-
-        return false;
+        return this.activeFlyout.isBlockCreatable(block);
     }
 
     scrollToStart(): void {
-        if (this.activeFlyout) {
-            this.activeFlyout.scrollToStart();
-        }
+        this.activeFlyout.scrollToStart();
     }
 
     getContents() {
-        return this.activeFlyout?.getContents() || [];
+        return this.activeFlyout.getContents();
     }
 
     protected blocksToString(xmlList: Element[]): string {
@@ -250,7 +137,6 @@ class CachedFlyout extends Blockly.VerticalFlyout {
         if (Array.isArray(flyoutDef)) {
             this.def = (flyoutDef as Element[]).slice();
         }
-
         super.show(flyoutDef);
     }
 
