71f0e1cb81b9546dda11cd19af8756d84d9bb8ea
Grace <grace@microbit.org>
2025-04-23 13:41:26 +0100

    Pull shortcuts from registry

--- webapp/src/components/KeyboardNavHelp.tsx
+++ webapp/src/components/KeyboardNavHelp.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { getActionShortcut, SHORTCUT_NAMES } from "../shortcut_formatting";
 
 const KeyboardNavHelp = () => {
     const ref = React.useRef<HTMLElement>(null);
@@ -7,283 +8,142 @@ const KeyboardNavHelp = () => {
     }, []);
     const cmd = pxt.BrowserUtils.isMac() ? "⌘" : "Ctrl";
     const optionOrCtrl = pxt.BrowserUtils.isMac() ? "⌥" : "Ctrl"
-    const enterOrSpace = <Shortcut value={["Enter", "Space"]} joinStyle="or" />
-    const contextMenu = <tr>
-        <td width="50%">Open context menu</td>
-        <td width="50%">
-            <Shortcut value={[cmd, "Enter"]} />
-        </td>
-    </tr>
-    const cleanUp = <tr>
-        {/* TODO: this needs to be reconciled with "Format Code" */}
-        <td width="50%">Workspace: Clean up</td>
-        <td width="50%">
-            <Shortcut value={["C"]} /><br />
-        </td>
-    </tr>
+    const contextMenuRow = <Row name="Open context menu" shortcuts={[SHORTCUT_NAMES.MENU]} />
+    // TODO: this needs to be reconciled with "Format Code"
+    const cleanUpRow = <Row name="Workspace: Clean up" shortcuts={[SHORTCUT_NAMES.CLEAN_UP]} />
+    const enterOrSpace = { shortcuts: [["Enter"], ["Space"]], joiner: "or"}
+    const editOrConfirmRow = <Row name="Edit or confirm" {...enterOrSpace} />
     return (
         <aside id="keyboardnavhelp" aria-label={lf("Keyboard Controls")} ref={ref} tabIndex={0}>
             <h2>Keyboard Controls</h2>
             <table>
                 <tbody>
-                    <tr>
-                        <td width="50%">Show/hide shortcut help</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "/"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Block and toolbox navigation
-                        </td>
-                        <td>
-                            <Shortcut value={["↑", "↓", "←", "→"]} joinStyle="none" /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Toolbox or insert</td>
-                        <td>
-                            <Shortcut value={["T", "I"]} joinStyle="or" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Edit or confirm</td>
-                        <td>
-                            {enterOrSpace}
-                        </td>
-                    </tr>
-                    <MoveModeRow>
+                    <Row name="Show/hide shortcut help" shortcuts={[SHORTCUT_NAMES.LIST_SHORTCUTS]} />
+                    <Row name="Block and toolbox navigation" shortcuts={[SHORTCUT_NAMES.UP, SHORTCUT_NAMES.DOWN, SHORTCUT_NAMES.LEFT, SHORTCUT_NAMES.RIGHT]} />
+                    <Row name="Toolbox or insert" shortcuts={[SHORTCUT_NAMES.TOOLBOX, SHORTCUT_NAMES.INSERT]} joiner="or" />
+                    {editOrConfirmRow}
+                    <Row name="Move mode" shortcuts={[["M"]]} >
                         <br /><span className="hint">Move with arrow keys</span>
                         <br /><span className="hint">Hold {optionOrCtrl} for free movement</span>
-                    </MoveModeRow>
-                    <tr>
-                        <td>Copy / paste</td>
-                        <td>
-                            <Shortcut value={[cmd, "C"]} /> / <Shortcut value={[cmd, "V"]} />
-                        </td>
-                    </tr>
-                    {cleanUp}
-                    {contextMenu}
+                    </Row>
+                    <Row name="Copy / paste" shortcuts={[SHORTCUT_NAMES.COPY, SHORTCUT_NAMES.PASTE]} joiner="/" />
+                    {cleanUpRow}
+                    {contextMenuRow}
                 </tbody>
             </table>
             <h3>Editor Overview</h3>
             <table>
                 <tbody>
-                    <tr>
-                        <td width="50%" rowSpan={2}>Move between menus, simulator and the workspace</td>
-                        <td width="50%">
-                            <Shortcut value={["Tab"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">
-                            <Shortcut value={["Shift", "Tab"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Exit</td>
-                        <td width="50%">
-                            <Shortcut value={["Escape"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Toolbox</td>
-                        <td width="50%">
-                            <Shortcut value={["T"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Toolbox: Move in and out of categories</td>
-                        <td width="50%">
-                            <Shortcut value={["←", "→"]} joinStyle="none" /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Toolbox: Navigate categories or blocks</td>
-                        <td width="50%">
-                            <Shortcut value={["↑", "↓"]} joinStyle="none" /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Toolbox: Insert block</td>
-                        <td width="50%">
-                            {enterOrSpace}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Workspace</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "E"]} /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Workspace: Select workspace</td>
-                        <td width="50%">
-                            <Shortcut value={["W"]} /><br />
-                        </td>
-                    </tr>
-                    {cleanUp}
-                    <tr>
-                        <td width="50%">Simulator</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "B"]} /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Download</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "D"]} /><br />
-                        </td>
-                    </tr>
+                    <Row name="Move between menus, simulator and the workspace" shortcuts={[["Tab"], ["Shift", "Tab"]]} joiner="row"/>
+                    <Row name="Exit" shortcuts={[SHORTCUT_NAMES.EXIT]} />
+                    <Row name="Toolbox" shortcuts={[SHORTCUT_NAMES.TOOLBOX]} />
+                    <Row name="Toolbox: Move in and out of categories" shortcuts={[SHORTCUT_NAMES.LEFT, SHORTCUT_NAMES.RIGHT]} />
+                    <Row name="Toolbox: Navigate categories or blocks" shortcuts={[SHORTCUT_NAMES.UP, SHORTCUT_NAMES.DOWN]} />
+                    <Row name="Toolbox: Insert block" {...enterOrSpace} />
+                    <Row name="Workspace" shortcuts={[[cmd, "E"]]} />
+                    <Row name="Workspace: Select workspace" shortcuts={[["W"]]} />
+                    {cleanUpRow}
+                    <Row name="Simulator" shortcuts={[[cmd, "B"]]} />
+                    <Row name="Download" shortcuts={[[cmd, "D"]]} />
                 </tbody>
             </table>
             <h3>Edit Blocks</h3>
             <table>
                 <tbody>
-                    <tr>
-                        <td width="50%">Move in and out of a block</td>
-                        <td width="50%">
-                            <Shortcut value={["←", "→"]} joinStyle="none" /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Edit or confirm</td>
-                        <td width="50%">
-                            {enterOrSpace}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Cancel or exit</td>
-                        <td width="50%">
-                            <Shortcut value={["Escape"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Insert block at current position</td>
-                        <td width="50%">
-                            <Shortcut value={["I"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Copy</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "C"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Paste</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "V"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Cut</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "X"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Delete</td>
-                        <td width="50%">
-                            <Shortcut value={["Delete"]} /> or <Shortcut value={["Backspace"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Undo</td>
-                        <td width="50%">
-                            <Shortcut value={[cmd, "Z"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Redo</td>
-                        <td width="50%">
-                            { pxt.BrowserUtils.isWindows() ? <Shortcut value={["Ctrl", "Y"]} /> : <Shortcut value={[cmd, "Shift", "Z"]} /> }
-                        </td>
-                    </tr>
-                    {contextMenu}
+                    <Row name="Move in and out of a block" shortcuts={[SHORTCUT_NAMES.LEFT, SHORTCUT_NAMES.RIGHT]} />
+                    {editOrConfirmRow}
+                    <Row name="Cancel or exit" shortcuts={[SHORTCUT_NAMES.EXIT]} />
+                    <Row name="Insert block at current position" shortcuts={[SHORTCUT_NAMES.INSERT]} />
+                    <Row name="Copy" shortcuts={[SHORTCUT_NAMES.COPY]} />
+                    <Row name="Paste" shortcuts={[SHORTCUT_NAMES.PASTE]} />
+                    <Row name="Cut" shortcuts={[SHORTCUT_NAMES.CUT]} />
+                    <Row name="Delete" shortcuts={[SHORTCUT_NAMES.DELETE, ["Backspace"]]} joiner="or" />
+                    <Row name="Undo" shortcuts={[SHORTCUT_NAMES.UNDO]} />
+                    <Row name="Redo" shortcuts={[SHORTCUT_NAMES.REDO]} />
+                    {contextMenuRow}
                 </tbody>
             </table>
             <h3>Moving Blocks</h3>
             <table>
                 <tbody>
-                    <MoveModeRow />
-                    <tr>
-                        <td>
-                            Move mode: Move to new position
-                        </td>
-                        <td>
-                            <Shortcut value={["↑", "↓", "←", "→"]} joinStyle="none" /><br />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Move mode: Free movement
-                        </td>
-                        <td>
-                            Hold <Key value={optionOrCtrl} /> and press arrow keys
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Move mode: Confirm
-                        </td>
-                        <td>
-                            {enterOrSpace}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Move mode: Cancel
-                        </td>
-                        <td>
-                            <Shortcut value={["Escape"]} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Disconnect blocks
-                        </td>
-                        <td>
-                            <Shortcut value={["X"]} />
-                        </td>
-                    </tr>
+                    <Row name="Move mode" shortcuts={[["M"]]} />
+                    <Row name="Move mode: Move to new position" shortcuts={[SHORTCUT_NAMES.UP, SHORTCUT_NAMES.DOWN, SHORTCUT_NAMES.LEFT, SHORTCUT_NAMES.RIGHT]} />
+                    <Row name="Move mode: Free movement">
+                        Hold <Key value={optionOrCtrl} /> and press arrow keys
+                    </Row>
+                    <Row name="Move mode: Confirm" {...enterOrSpace} />
+                    <Row name="Move mode: Cancel" shortcuts={[SHORTCUT_NAMES.EXIT]} />
+                    <Row name="Disconnect blocks" shortcuts={[SHORTCUT_NAMES.DISCONNECT]} />
                 </tbody>
             </table>
         </aside>
     );
 }
 
-type JoinStyle = "and" | "none" | "or"
-const Shortcut = ({ value, joinStyle = "and" }: { value: string[], joinStyle?: JoinStyle }) => {
-    const joiners: Record<JoinStyle, string> = {
-        "and": pxt.BrowserUtils.isMac() ? " " : " + ",
-        "or": " or ",
-        "none": " "
-    }
+const Shortcut = ({ keys }: { keys: string[] }) => {
+    const joiner = pxt.BrowserUtils.isMac() ? " " : " + "
     return (
         <span className="shortcut">
-            {value.reduce((acc, v) => {
+            {keys.reduce((acc, key) => {
                 return acc.length === 0
-                    ? [...acc,  <Key key={v} value={v} />]
-                    : [...acc, joiners[joinStyle], <Key key={v} value={v} />]
+                    ? [...acc,  <Key key={key} value={key} />]
+                    : [...acc, joiner, <Key key={key} value={key} />]
             }, [])}
         </span>
     );
 }
 
+interface RowProps {
+    name: string;
+    shortcuts?: Array<string | string[]>;
+    joiner?: string;
+    children?: React.ReactNode;
+}
+
+const Row = ({ name, shortcuts = [], joiner, children}: RowProps) => {
+    const shortcutElements = shortcuts.map(s => {
+        if (typeof s === "string") {
+            // Pull keys from shortcut registry.
+            return <Shortcut keys={getActionShortcut(s)} />
+        } else {
+            // Display keys as specified.
+            return <Shortcut keys={s} />
+        }
+    })
+    return joiner === "row" ? (
+        <>
+            <tr>
+                <td width="50%" rowSpan={shortcuts.length}>Move between menus, simulator and the workspace</td>
+                <td width="50%">
+                    {shortcutElements[0]}
+                </td>
+            </tr>
+            {shortcutElements.map((el, idx) => idx === 0
+                ? undefined
+                : (<tr>
+                    <td width="50%">
+                        {el}
+                    </td>
+                </tr>))}
+        </>
+    ) : (
+        <tr>
+            <td width="50%">{name}</td>
+            <td width="50%">
+                {shortcutElements.reduce((acc, shortcut) => {
+                    return acc.length === 0
+                        ? [...acc,  shortcut]
+                        : [...acc, joiner ? ` ${joiner} ` : " ", shortcut]
+                }, [])}
+                {children}
+                <br />
+            </td>
+        </tr>
+    )
+}
 
 const Key = ({ value }: { value: string }) => {
     return <span className="key">{value}</span>
 }
-
-const MoveModeRow = ({ children }: { children?: React.ReactNode}) => (
-    <tr>
-        <td>Move mode</td>
-        <td>
-            <Shortcut value={["M"]} />
-            {children}
-        </td>
-    </tr>
-)
 
 export default KeyboardNavHelp;
