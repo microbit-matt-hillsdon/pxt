134bfe7082e5f38516ad42f68f7f252de5969fbc
Alex Shaw <alex.shaw@microbit.org>
2025-05-02 16:35:42 +0100

    2-column gallery in Sound Effect editor

--- webapp/src/components/soundEffectEditor/SoundGallery.tsx
+++ webapp/src/components/soundEffectEditor/SoundGallery.tsx
@@ -18,26 +18,63 @@ export interface SoundGalleryProps {
 
 interface SoundGalleryItemProps extends SoundGalleryItem {
     useMixerSynthesizer: boolean;
+    onClick: () => void;
+    selectReference: (el: HTMLDivElement) => void;
+    playReference: (el: HTMLButtonElement) => void;
+    previewKeyDown: (evt: React.KeyboardEvent<HTMLElement>) => void;
+    selectKeyDown: (evt: React.KeyboardEvent<HTMLElement>) => void;
 }
 
 export const SoundGallery = (props: SoundGalleryProps) => {
     const { sounds, onSoundSelected, visible, useMixerSynthesizer } = props;
 
-    const soundItemRefs = React.useRef<Record<string,HTMLElement>>({});
+    const selectItemRefs = React.useRef<Record<string,HTMLElement>>({});
+    const playItemRefs = React.useRef<Record<string,HTMLElement>>({});
 
-    function listNav(
+    function selectNav(
         prev: number,
         next: number,
+        current: number,
         event: React.KeyboardEvent<HTMLElement>) {
-            if (event.code === "ArrowDown") {
-                soundItemRefs.current[next].focus();
+            console.log("Selectnav", event.code);
+        switch(event.code) {
+            case "ArrowDown":
+                selectItemRefs.current[next].focus();
+                console.log("focused ",selectItemRefs.current, next);
                 event.preventDefault();
-            } else if (event.code === "ArrowUp") {
-                soundItemRefs.current[prev].focus();
+                break;
+            case "ArrowUp":
+                selectItemRefs.current[prev].focus();
                 event.preventDefault();
-            } else {
-                fireClickOnEnter(event);
-            }
+                break;
+            case "ArrowRight":
+                playItemRefs.current[current].focus();
+                event.preventDefault();
+                break;
+            default:
+        }
+    }
+
+    function previewNav(
+        prev: number,
+        next: number,
+        current: number,
+        event: React.KeyboardEvent<HTMLElement>) {
+        switch(event.code) {
+            case "ArrowDown":
+                playItemRefs.current[next].focus();
+                event.preventDefault();
+                break;
+            case "ArrowUp":
+                playItemRefs.current[prev].focus();
+                event.preventDefault();
+                break;
+            case "ArrowLeft":
+                selectItemRefs.current[current].focus();
+                event.preventDefault();
+                break;
+            default:
+        }
     }
 
     return <div className={classList("sound-gallery", visible && "visible")} aria-hidden={!visible}>
@@ -47,15 +84,20 @@ export const SoundGallery = (props: SoundGalleryProps) => {
                     const next = (index + 1) % sounds.length;
                     return(<div
                         key={index}
-                        ref={ref => soundItemRefs.current[index] = ref}
-                        className="common-button"
-                        title={item.name}
-                        role="button"
-                        tabIndex={0}
-                        onKeyDown={e => listNav(prev, next, e)}
-                        onClick={() => onSoundSelected(item.sound)}>
+                        onClick={() => onSoundSelected(item.sound)}
+                        className="common-button">
 
-                        <SoundGalleryEntry {...item} useMixerSynthesizer={useMixerSynthesizer} />
+                        <SoundGalleryEntry
+                            {...item}
+                            useMixerSynthesizer={useMixerSynthesizer}
+                            onClick={() => onSoundSelected(item.sound)}
+
+                            playReference={ref => playItemRefs.current[index] = ref}
+                            selectReference={ref => selectItemRefs.current[index] = ref}
+
+                            previewKeyDown={evt => previewNav(prev, next, index, evt)}
+                            selectKeyDown={evt => selectNav(prev, next, index, evt)}
+                        />
                     </div>);
                 })
             }
@@ -64,7 +106,16 @@ export const SoundGallery = (props: SoundGalleryProps) => {
 }
 
 const SoundGalleryEntry = (props: SoundGalleryItemProps) => {
-    const { sound, name, useMixerSynthesizer } = props;
+    const { 
+        sound,
+        name,
+        onClick,
+        useMixerSynthesizer,
+        playReference,
+        selectReference,
+        previewKeyDown,
+        selectKeyDown
+    } = props;
     const width = 160;
     const height = 40;
 
@@ -92,22 +143,32 @@ const SoundGalleryEntry = (props: SoundGalleryItemProps) => {
     }
 
     return <div className="sound-gallery-item-label">
-        <div className="sound-effect-name">
-            {name}
-        </div>
-        <div className="sound-gallery-preview">
-            <svg viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
-                <path
-                    className="sound-gallery-preview-wave"
-                    d={pxt.assets.renderSoundPath(sound, width, height)}
-                    strokeWidth="2"
-                    fill="none"/>
-            </svg>
+        <div className="sound-gallery-item-label-inner"
+            tabIndex={0}
+            ref={selectReference}
+            onClick={onClick}
+            onKeyDown={selectKeyDown}
+            title={name}
+            role="button">
+            <div className="sound-effect-name">
+                {name}
+            </div>
+            <div className="sound-gallery-preview">
+                <svg viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        className="sound-gallery-preview-wave"
+                        d={pxt.assets.renderSoundPath(sound, width, height)}
+                        strokeWidth="2"
+                        fill="none"/>
+                </svg>
+            </div>
         </div>
         <Button
             className="sound-effect-play-button"
+            buttonRef={playReference}
             title={cancelToken ? lf("Stop Sound Preview") : lf("Preview Sound")}
             onClick={handlePlayButtonClick}
+            onKeydown={previewKeyDown}
             leftIcon={cancelToken ? "fas fa-stop" : "fas fa-play"}
             />
     </div>
