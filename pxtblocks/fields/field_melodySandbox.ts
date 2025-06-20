60d61a0cf65ca7cc90e3b82d0b3cadf08fc81ca4
Alex Shaw <alex.shaw@microbit.org>
2025-06-20 09:43:17 +0100

    Simple down-arrow implementation as it is a common pattern accessing galleries

--- pxtblocks/fields/field_melodySandbox.ts
+++ pxtblocks/fields/field_melodySandbox.ts
@@ -173,12 +173,15 @@ export class FieldCustomMelody<U extends FieldCustomOptions> extends FieldMatrix
         // Same toggle set up as sprite editor
         this.root = new svg.SVG(this.topDiv).id("melody-editor-header-controls");
         this.toggle = new Toggle(this.root, { leftText: lf("Editor"), rightText: lf("Gallery"), baseColor: color });
-        this.toggle.onStateChange(isLeft => {
+        this.toggle.onStateChange((isLeft, isDown) => {
             if (isLeft) {
                 this.hideGallery();
             }
             else {
                 this.showGallery();
+                if (isDown) {
+                    this.lastFocusableElement.focus();
+                }
             }
         });
         this.firstFocusableElement = this.toggle.getRootElement();
@@ -761,7 +764,7 @@ class Toggle {
     protected props: ToggleProps;
 
     protected isLeft: boolean;
-    protected changeHandler: (left: boolean) => void;
+    protected changeHandler: (left: boolean, down: boolean) => void;
 
     constructor(parent: svg.SVG, props: Partial<ToggleProps>) {
         this.props = defaultColors(props);
@@ -858,6 +861,11 @@ class Toggle {
                 this.toggle();
                 e.preventDefault();
             }
+            if (e.code === "ArrowDown" && !this.isLeft) {
+                console.log("ArrowDown");
+                this.changeHandler(false, true);
+                e.preventDefault();
+            }
         });
     }
 
@@ -877,11 +885,11 @@ class Toggle {
         this.isLeft = !this.isLeft;
 
         if (!quiet && this.changeHandler) {
-            this.changeHandler(this.isLeft);
+            this.changeHandler(this.isLeft, false);
         }
     }
 
-    onStateChange(handler: (left: boolean) => void) {
+    onStateChange(handler: (left: boolean, down: boolean) => void) {
         this.changeHandler = handler;
     }
 
